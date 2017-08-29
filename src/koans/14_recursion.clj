855da834e030dd49abc33342f0a257f898211d43
KaFai Choi <kafaicoder@gmail.com>
2017-08-29 18:30:56 +0700

    finish up to ch21

--- src/koans/14_recursion.clj
+++ src/koans/14_recursion.clj
@@ -3,21 +3,32 @@
 
 (defn is-even? [n]
   (if (= n 0)
-    __
-    (___ (is-even? (dec n)))))
+    true
+    (not (is-even? (dec n)))))
 
 (defn is-even-bigint? [n]
   (loop [n   n
          acc true]
     (if (= n 0)
-      __
+      false
       (recur (dec n) (not acc)))))
 
 (defn recursive-reverse [coll]
-  __)
+  (loop [coll coll
+         rev '()]
+    (if (= '() coll)
+      rev
+      (recur (rest coll) (cons (first coll) rev) )
+    )))
 
 (defn factorial [n]
-  __)
+  (loop [n n
+         r 1]
+    (if (= n 1)
+      r
+    (recur (dec n) (* n r))
+    )
+  ))
 
 (meditations
   "Recursion ends with a base case"
